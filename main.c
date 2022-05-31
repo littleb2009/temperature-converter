b02a1dc74939831f136fee71b8ca4213c3ca642e
littleb2009 <102564844+littleb2009@users.noreply.github.com>
2022-05-31 15:35:06 -0700

    Prints usage. Uppercase temperature scale support.
    Prints the usage of the program upon error or if temperature scale is "h".
    You can now use F, C, and K for Fahrenheit, Celsius, and Kelvin, respectively.

--- main.c
+++ main.c
@@ -8,34 +8,61 @@ const long double CTOK = 273.15l;
 
 long double celsius, kelvin, fahrenheit;
 
+const char usage[] =
+  "Usage: tempconvert scale value\n"
+  "\n"
+  "scale: The temperature scale the value is provided in:\n"
+  "  f, F - Fahrenheit\n"
+  "  c, C - Celsius\n"
+  "  k, K - Kelvin\n"
+  "  r, R - Rankine (under development)\n"
+  "\n"
+  "value: The numeric value.\n";
+
+
 int main(int argc, char **argv) {
-  if (argc != 3) return 1;
+  if(argv[1][0]=='h') {
+    printf("%s", usage);
+    return 0;
+  }
+
+  if (argc != 3) {
+    fprintf(stderr, "Bad argument count.\n");
+    printf("%s", usage);
+    return 1;
+  }
 
   char *endptr = (char *)malloc(4096 * sizeof(char));
   switch (argv[1][0]) {
     case 'f':
+    case 'F':
       fahrenheit = strtold(argv[2], &endptr);
       celsius = ftoc(fahrenheit);
       kelvin = celsius + CTOK;
 
       break;
     case 'c':
+    case 'C':
       celsius = strtold(argv[2], &endptr);
       fahrenheit = ctof(celsius);
       kelvin = celsius + CTOK;
 
       break;
     case 'k':
+    case 'K':
       kelvin = strtold(argv[2], &endptr);
       celsius = kelvin - CTOK;
       fahrenheit = ctof(celsius);
 
       break;
     case 'r':
+    case 'R':
       return 1;
 
       break;
     default:
+      fprintf(stderr, "Unrecognized temperature scale %c\n", argv[1][0]);
+      printf("%s", value);
       return 1;
   }
   printf("K: %.15Lf\nC: %.15Lf\nF: %.15Lf\n", kelvin, celsius, fahrenheit);
